faf9c8aa2f0bd52c6e2464f0d66c6e97876d6598
Eric Kenneth Anderson <eric.k.anderson@intel.com>
2022-09-01 13:27:40 -0700

    Initial commit of synthetic data loader.   Not fully incorporated.

---
+++ loaders/synthetic_loader/loader.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import make_regression
+from datetime import datetime, timedelta
+
+def datetime_to_pathlike_string(dt):
+    return dt.strftime("%Y%m%d-%H%M%S")
+
+def create_synthetic_data(observation_count=1e6):
+    n_features=2000
+    n_samples = observation_count
+    n_informative=10
+    n_targets = 2
+    effective_rank=100
+    tail_strength = 0.5
+    noise=0.1
+
+    X, y = make_regression(n_features=n_features, n_samples=n_samples, n_informative=n_informative,
+                           n_targets=n_targets, effective_rank=effective_rank, tail_strength=tail_strength, noise=noise)
+
+    print(X)
+    feature_names = [f"fcol`feature_{x}" for x in range(X.shape[1])]
+    df = pd.DataFrame(data = X, columns=feature_names)
+
+    print(df.head())
+
+    lots = []
+    wafers = []
+    lot_counter = 0
+    wafer_counter=0
+    for idx in range(df.shape[0]):
+        if idx % 25 == 0:
+            lot_counter += 1
+            wafer_counter = 0
+        lcounterstring = str(lot_counter)
+        lcounterstring = lcounterstring.rjust(10, '0')
+        lotstring = f"DG{lcounterstring}"
+        lots.append(lotstring)
+        wafers.append(wafer_counter)
+        wafer_counter += 1
+
+    df['LOT7'] = lots
+    df['WAFER3'] = wafers
+    df['PROCESS'] = "1234"
+    df['SHORTDEVICE'] = "DPMLD"
+
+
+
+
+    tostack = []
+    #['PROCESS', 'DEVREVSTEP', 'LOT7', 'WAFER3', 'TEST_END_DATE']
+    print("started_stacking")
+    for col in feature_names:
+        dff = df.loc[:, ['LOT7', 'WAFER3', 'PROCESS', 'SHORTDEVICE',  col]]
+        dff['TESTNAME`STRUCTURE_NAME'] = col
+        # dff['PROCES'] = "1234"
+        # dff['SHORTDEVICE'] = "DPMLD"
+        dff = dff.rename(columns = {col: 'TESTNAME`STRUCTURE_NAME'})
+        tostack.append(dff)
+
+    data = pd.concat(tostack)
+
+    start = datetime.now() - timedelta(days=90)
+    end = datetime.now()
+    dt = (end - start)/data.shape[0]
+
+    times = [start + dt*idx for idx in range(data.shape[0])]
+    data['TEST_END_DATE'] = times
+
+    parse_response_df(df, y)
+
+def parse_response_df(feature_df, y):
+    colnames = [f'response_{x}' for x in range(y.shape[1])]
+    df = pd.DataFrame(data = y, columns=colnames)
+
+    df = pd.concat([df, feature_df.loc[:, ['LOT7', 'WAFER3', 'SHORTDEVICE', 'PROCESS']]], axis=1)
+    print(df.head())
+
+def store_raw_file_et(data_df, params):
+    load_start = datetime_to_pathlike_string(datetime.now() - timedelta(days=90))
+    load_end = datetime_to_pathlike_string(datetime.now())
+    #fname = params['storage_path'] + f"/{load_start}--{load_end}.parquet"
+    fname = "/data/features" + f"/LOAD_END={load_end}"
+    data_df = data_df.reset_index()
+
+    data_df.to_parquet(fname, partition_cols=['PROCESS','OPERATION', 'SHORTDEVICE', 'LOT7'])
+
+def store_raw_file_sp(data_df, params):
+    load_start = datetime_to_pathlike_string(datetime.now() - timedelta(days=90))
+    load_end = datetime_to_pathlike_string(datetime.now())
+    fname = "/data/response" + f"/LOAD_END={load_end}"
+    data_df.reset_index().to_parquet(fname, partition_cols=['SHORTDEVICE', 'OPERATION'])
+
+if __name__ == "__main__":
+    create_synthetic_data(1000)
