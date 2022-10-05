39de827ae45c7aca022525d0e9335028755e780f
Eric Kenneth Anderson <eric.k.anderson@intel.com>
2022-10-05 10:16:00 -0700

    Fixed types on pyarrow filter criterion.

--- src/benchmark/benchmark.py
+++ src/benchmark/benchmark.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from os import environ
 import pandas as pd
 import numpy as np
-#get my imports
+#local imports
 import src.analytics.datasets.sort_parametrics as sortparam_dataset
 import src.analytics.datasets.etest as etest_dataset
 import src.analytics.pipelines.et_pipeline as et_pipeline
@@ -16,26 +16,29 @@ if environ.get('OUTPUT_DIR') is not None:
     etest_dir = rpath + "/data/synthetic_etest"
 
 
-def run_benchmark(devices='DPMLD', token_0='response_0', token_0_op='1234',
-                  token_1='response_1', token_1_op='1234', feature_op='6543'):
+def run_benchmark(devices='DPMLD', token_0='response_0', token_0_op=1234,
+                  token_1='response_1', token_1_op=1234, feature_op=6543):
     end_date = datetime.now()
     start_date = end_date - timedelta(days=45)
     data = sortparam_dataset.load_sort_parametric(sort_param_dir, devices, start_date, end_date, token_0, token_0_op,
                                                   token_1, token_1_op)
     data = data.reset_index()
-
+    print(data.head())
     edata = etest_dataset.load_etest_by_lotlist(etest_dir, data['LOT7'].unique(), feature_op)
 
     alldata = pd.merge(data, edata, on=['LOT7', 'WAFER3'], how='inner')
 
     sort_dt = (alldata['TEST_END_DATE_x'] - alldata['TEST_END_DATE_y']).median()
 
-    prediction_data = etest_dataset.load_etest(etest_dir, devices, etest_op,
+    prediction_data = etest_dataset.load_etest(etest_dir, devices, feature_op,
                                                data['TEST_END_DATE'].max() - sort_dt, datetime.now())
 
+    pipeline_start = datetime.now()
     data, alldata, prediction_data, fi_fmax, fi_sicc = et_pipeline.get_model(data, alldata, prediction_data, edata,
                                                                              token_0, token_1)
+    pipeline_end = datetime.now()
 
+    print(f"total pipeline time: {(pipeline_end - pipeline_start).total_seconds()}")
 
 if __name__ == "__main__":
     run_benchmark()
